7b2b0752cbf8fe077095f7adbd158c21aca89445
HugoBA <hugo.barattolo@hotmail.fr>
2013-09-26 12:35:26 +0200

    Began defining Boat class Added TODO's about a new type to create, in order to locate boats

---
+++ src/fr/iutvalence/java/mp/navalbattle/Boat.java
@@ -0,0 +1,39 @@
+package fr.iutvalence.java.mp.navalbattle;
+
+/**
+ * Represents each boat
+ * @author barattoh
+ *
+ */
+public class Boat
+{
+    /**
+     * Table which contains every positions of the boat on the grid
+     */
+    private int[] boatCases; // TODO créer un type coordonnées -> (x,y)
+    
+    /**
+     * Length of the boat
+     */
+    private int length;
+    
+    /**
+     * State of the boat, to know if it is sunk or not
+     */
+    public boolean sunk; 
+    
+    /**
+     * Makes the boat : initializes boat positions, and length
+     * @param extrem1 : first extremity of the boat
+     * @param extrem2 : second extremity of the boat
+     */
+    public Boat (int extrem1, int extrem2)
+    {
+        int xa, xb, ya, yb;
+        xa = extrem1 / 10; ya = extrem1 % 10; //TODO revoir calcul avec nouveau type coordonnees
+        xb = extrem2 / 10; yb = extrem2 % 10;
+        
+        this.length = java.lang.Math.abs((xa - xb) + (ya - yb));
+        this.sunk = false;
+    }
+}
